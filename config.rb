7212324d6b0617795446790407938ffccfbd599a
Gary Hepting <engineering+gary@sidereel.com>
2013-04-04 17:22:36 -0700

    fixed compass config

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "groundwork/css"
+sass_dir = "groundwork/src/scss"
+images_dir = "groundwork/images"
+javascripts_dir = "groundwork/js"
+fonts_dir = "groundwork/font"
+
+output_style = :expanded
+environment = :development
+
+relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass groundwork/src/scss scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
